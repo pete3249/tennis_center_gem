cfd6445696b349c9ebe3319723fe1d8b0c0257b1
pete3249 <katelynrpeterson16@gmail.com>
2020-07-13 12:45:50 -0400

    Update methods

--- lib/tennis_center_gem/cli.rb
+++ lib/tennis_center_gem/cli.rb
@@ -2,21 +2,26 @@ require 'pry'
 
 module TennisCenterGem
    class CLI
+
+        def initialize
+            @input = " "
+        end 
+
         def start
             puts "Welcome to the Tennis Center Gem!"
+            set_location
+            list_tennis_centers
+        end
+
+        def set_location
             puts "Please enter your location."
             @location = gets.strip
             TennisCenter.load_by_location(@location)
         end 
 
+        def list_tennis_centers
+            TennisCenter.all.each.with_index(1) {|tennis_center, index| puts "#{index}. #{tennis_center.name}"}
+        end 
+
     end     
 end 
-
-
-
-
-    # def display
-    #     print_tennis_centers
-    #     puts "Which Tennis Center would you like more information on?"
-    #     input = gets.strip
-    # end 
--- lib/tennis_center_gem/tennis_center.rb
+++ lib/tennis_center_gem/tennis_center.rb
@@ -1,62 +1,30 @@
 module TennisCenterGem
     class TennisCenter
-        attr_accessor :name, :url, :rating, :location, :phone_number, :id
+        attr_accessor :name, :url, :rating, :location, :display_phone, :id
         @@all = []
 
-        def initialize
-
+        def initialize(attributes = {})
+            attributes.each do |attribute_name, value| 
+                if self.respond_to?("#{attribute_name}=")
+                    self.send(("#{attribute_name}="), value)
+                end 
+            end   
         end 
 
         def self.load_by_location(location)
             search_results = API.yelp_search("Tennis Center", location)
-            create_from_API_results(search_results)
+            @@all = create_from_API_results(search_results)
         end 
 
         def self.create_from_API_results(search_results)
-            
-        end 
-
-
-
-
-        def new_from_api_data(t, index)
-            self.new(
-                new_hash["businesses"][index]["name"],
-                new_hash["businesses"][index]["url"],
-                new_hash["businesses"][index]["rating"],
-                new_hash["businesses"][index]["location"]["address1"],
-                #phone_number,
-                #id
-                #not sure if index is needed or can just use #each
-            )
-        end 
-        
-        
-        # def initialize(name = nil, url = nil, rating = nil, location = nil, phone_number = nil, id = nil)
-        #     @name = name
-        #     @url = url
-        #     @rating = rating
-        #     @location = location
-        #     @phone_number = phone_number
-        #     @id = id
-        #     @@all << self
-        # end 
-
-        def display_tennis_center_from_number(number)
-
-        end 
-
-
-         
-
-
-
-
-        def self.make_tennis_centers
-            yelp_search.each.with_index do |t, index|
-                TennisCenter.new_from_api_data(t, index)
+            search_results.collect do |tennis_center_hash|
+                TennisCenter.new(tennis_center_hash)
             end 
         end 
-        
+
+        def self.all
+            @@all
+        end 
+
     end 
 end 
